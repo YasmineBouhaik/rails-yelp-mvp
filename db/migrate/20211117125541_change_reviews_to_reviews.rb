53dcbcf4e021804e6582ed2faed7b934ae95613f
YasmineBouhaik <yasmine.bouhaik@gmail.com>
2021-11-17 16:55:54 +0100

    rails yelp mvp

---
+++ db/migrate/20211117125541_change_reviews_to_reviews.rb
@@ -0,0 +1,5 @@
+class ChangeReviewsToReviews < ActiveRecord::Migration[6.0]
+  def change
+    change_column :reviews, :rating, :integer
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dinette = { name: "Dinette", address: "rue Saint-André, Lille", category: 'french' }
+papa =  { name: "Papa Pizza", address: "rue de la Monnaie, Lille", category: 'italian' }
+
+[ dinette, papa ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
